e942318932c220c57f484349fc1bd0cc181f2831
jwhitish <jacobwhitish@gmail.com>
2018-02-17 17:09:01 -0800

    odin project test driven assignment

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+def add(x,y)
+  sum = x + y
+  return sum
+end
+
+def subtract(x,y)
+  delta = x - y
+  return delta
+end
+
+def sum(x)
+  z = 0
+  x.each {|item| z += item}
+  return z
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,27 @@
 #write your code here
+
+def echo(word)
+  return word
+end
+
+def shout(word)
+  return word.upcase
+end
+
+def repeat(word, num=2)
+  array = []
+  n = num.to_i
+  n.times do
+    array.push word
+  end
+  str = array.join(" ").to_s
+  return str
+end
+
+def start_of_word(word, num=0)
+  word_array = []
+  word.each_char { |chr| word_array.push(chr) }
+  return word_array[num-1]
+end
+
+#start_of_word("hello", 2)
