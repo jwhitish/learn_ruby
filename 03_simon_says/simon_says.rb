a2e90af70daa44e9917eada77877696ddc1712af
jwhitish <jacobwhitish@gmail.com>
2018-02-18 11:37:02 -0800

    completed simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -21,7 +21,28 @@ end
 def start_of_word(word, num=0)
   word_array = []
   word.each_char { |chr| word_array.push(chr) }
-  return word_array[num-1]
+  return word_array[0..num-1].join
 end
 
-#start_of_word("hello", 2)
+def first_word(sentence)
+  word_array = sentence.split(" ")
+  return word_array[0]
+end
+
+def titleize(words)
+  word_array = words.split
+  final_array = []
+  word_array.each do |word|
+    if word == 'and' || word == 'or' || word == 'a' || word == 'the' || word == 'over'
+      final_array.push word
+    else
+      final_array.push word.capitalize
+    end
+  end
+  final_array[0].capitalize!
+  y = final_array.join(' ')
+  return y
+end
+
+#titleize('the bridge over the river kwai')
+#answer - The Bridge over the River Kwai
