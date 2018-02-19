d92d4d39f95e35e716389eb3586e301aeb635485
jwhitish <jacobwhitish@gmail.com>
2018-02-18 20:02:38 -0800

    completed test driven exercises

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+  attr_accessor :title
+
+  def title= (name)
+    exceptions = ['and', 'in', 'or', 'but', 'for', 'nor', 'so', 'of', 'the', 'a', 'an']
+    words = name.split(" ")
+    words.each do |word|
+      if exceptions.include?(word) == FALSE
+        word.capitalize!
+      end
+      words[0].capitalize!
+      @title = words.join(" ").to_s
+    end
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  attr_accessor :timer
+
+  def initialize
+    @seconds = 00
+  end
+
+  @timer = Timer.new
+  def time_string
+    return Time.at(@seconds).utc.strftime("%H:%M:%S")
+  end
 end
+
+#Timer.new.time_string
